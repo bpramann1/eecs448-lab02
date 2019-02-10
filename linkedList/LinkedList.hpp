cc9e6f5edff42f29fae210d226ad64520635a78f
Brandon Pramann <bpramann1@gmail.com>
2019-02-09 20:50:19 -0600

    Fix removeBack() Method
    Fix removeBack() Method by first checking to see if the list has is nodes farther and then removes the back most node.

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -105,10 +105,22 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO 
-		Fix this method
-	*/
-
+	if (!isEmpty())
+	{
+		lastNode = m_front;
+		while (lastNode->getNext() nullptr)
+		{
+			secondintoLast = lastNode;
+			lastNode = lastNode->getNext();
+		}
+		delete lastNode;
+		if (secondintoLast != nullptr)
+		{
+			secondintoLast->setNext(nullptr);
+		}
+		m_size--;
+		isRemoved = true;
+	}
 	return(isRemoved);
 }	
 
